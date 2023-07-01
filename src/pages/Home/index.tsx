7fd4ba10c7fe496a00e560892e344c4f93bc85a3
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2023-07-01 09:56:55 -0300

    Estrutura Visual Completa

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,10 @@
-import { CarrosselContainer, CategorySection, Container, FooterConteiner, NavContainer } from './styles'
+import { CarrosselContainer, CategorySection, Container, FacilitieBox, FacilitiesArea, FooterConteiner, NavContainer, NewsLetterArea, ProductsArea } from './styles'
 
 import meteoraLogo from '../../assets/meteora-logo.svg'
 import Banner1 from '../../assets/banners/banner-1.png'
+import pixImg from '../../assets/facilites/pix.svg'
+import changeImg from '../../assets/facilites/change.svg'
+import flowerImg from '../../assets/facilites/flower.svg'
 
 export function Home() {
 
@@ -61,6 +64,57 @@ export function Home() {
         }
     ]
 
+    const productsList = [
+        {
+            id: '1',
+            image: '/src/assets/products/Camiseta.png',
+            title: 'Camiseta Conforto',
+            description: 'Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o verão. Modelagem unissex.',
+            category: 'Camisetas',
+            price: 70
+        },
+        {
+            id: '2',
+            image: '/src/assets/products/Calça.png',
+            title: 'Calça Alfaiataria',
+            description: 'Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!',
+            category: 'Calças',
+            price: 180
+        },
+        {
+            id: '3',
+            image: '/src/assets/products/Tenis.png',
+            title: 'Tênis Chunky',
+            description: 'Snicker casual com solado mais alto e modelagem robusta. Modelo unissex.',
+            category: 'Calçados',
+            price: 250
+        },
+        {
+            id: '4',
+            image: '/src/assets/products/Jaqueta.png',
+            title: 'Jaqueta Jeans',
+            description: 'Modelo unissex oversized com gola de camurça. Atemporal e autêntica!',
+            category: 'Casacos',
+            price: 150
+        },
+        {
+            id: '5',
+            image: '/src/assets/products/óculos.png',
+            title: 'Óculos Redondo',
+            description: 'Armação metálica em grafite com lentes arredondadas. Sem erro!',
+            category: 'Óculos',
+            price: 120
+        },
+        {
+            id: '6',
+            image: '/src/assets/products/Bolsa.png',
+            title: 'Bolsa coringa',
+            description: 'Bolsa camel em couro sintético de alta duração. Ideal para acompanhar você por uma vida!',
+            category: 'Bolsas',
+            price: 120
+        },
+    ]
+
     return (
         <>
             <NavContainer>
@@ -118,6 +172,80 @@ export function Home() {
 
                 </section>
             </CategorySection>
+
+            <ProductsArea>
+                <h1>Produtos que estão bombando!</h1>
+
+                <section>
+                    {productsList.map(product => {
+
+                        const priceFormated = new Intl.NumberFormat('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL'
+                        }).format(product.price)
+
+                        return (
+                            <div key={product.id}>
+
+                                <img src={product.image} alt='imagem do produto' />
+
+                                <footer>
+
+                                    <h3>{product.title}</h3>
+
+                                    <p>{product.description}</p>
+
+                                    <div>
+                                        <span>{priceFormated}</span>
+                                        <button>Ver mais</button>
+                                    </div>
+
+                                </footer>
+
+                            </div>
+                        )
+                    })}
+                </section>
+            </ProductsArea>
+
+            <FacilitiesArea>
+                <h1>Conheça todas as nossas facilidades</h1>
+
+                <section>
+                    <FacilitieBox>
+                        <img src={pixImg} />
+                        <div>
+                            <span>PAGUE PELO PIX</span>
+                            <p>Ganhe 5% OFF empagamentos via PIX.</p>
+                        </div>
+                    </FacilitieBox>
+                    <FacilitieBox>
+                        <img src={changeImg} />
+                        <div>
+                            <span>TROCA GRÁTIS</span>
+                            <p>Fique livre para trocar em até 30 dias.</p>
+                        </div>
+                    </FacilitieBox>
+                    <FacilitieBox>
+                        <img src={flowerImg} />
+                        <div>
+                            <span>SUSTENTABILIDADE</span>
+                            <p>Moda responsável, que respeita o meio ambiente.</p>
+                        </div>
+                    </FacilitieBox>
+                </section>
+            </FacilitiesArea>
+
+            <NewsLetterArea>
+
+                <h2>Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? Cadastre-se!</h2>
+                
+                <form>
+                    <input type="text" placeholder='Digite seu email' />
+                    <button>Enviar</button>
+                </form>
+
+            </NewsLetterArea>
 
             <FooterConteiner>
                 <span>2023 © Desenvolvido por Jhones | Projeto fictício sem fins comerciais.</span>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -48,17 +48,17 @@ export const Container = styled.div`
 export const CarrosselContainer = styled.div`
     width: 100vw;
     text-align: center;
-    margin-top: -3px;
+    margin-top: -2px;
 
     img{
-        width: 100%;
+        max-width: 1220px;
     }
 `
 
 export const CategorySection = styled.div`
     h1{
         text-align: center;
-        margin: 2rem 0;
+        margin: 3rem 0 2rem;
     }
 
     section{
@@ -71,6 +71,7 @@ export const CategorySection = styled.div`
         div{
 
             background-color: ${props => props.theme['--green']};
+            cursor: pointer;
 
             img{
                 max-height: 8rem;
@@ -86,11 +87,133 @@ export const CategorySection = styled.div`
     }
 `
 
+export const ProductsArea = styled.div`
+    max-width: 1220px;
+    margin: 0 auto;
+
+    h1{
+        margin: 4rem 0 2rem;
+        text-align: center;
+    }
+
+    section{
+        display: flex;
+        flex-wrap: wrap;
+        gap: 2rem;
+        justify-content: space-between;
+
+        div{
+            max-width: 22rem;
+            border: 1px solid ${props => props.theme['--gray']}
+        }
+
+        footer{
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+            padding: 1rem;
+
+            p{
+                height: 4rem;
+            }
+
+            div{
+                border: 0;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+
+                span{
+                    font-weight: bold;
+                    font-size: 1.25rem;
+                }
+
+                button{
+                    border: 0;
+                    background-color: ${props => props.theme['--purple']};
+                    padding: 0.75rem;
+                    color: ${props => props.theme['--white']};
+                    font-weight: bold;
+                    cursor: pointer;
+                }
+            }
+        }
+    }
+`
+
+export const FacilitiesArea = styled.div`
+    margin: 4rem 0 0;
+    background-color: ${props => props.theme['--black']};
+    color: ${props => props.theme['--white']};
+    padding: 3rem 0;
+
+    h1{
+        text-align: center;
+    }
+
+    section{
+        max-width: 1220px;
+        margin: 0 auto;
+        display: flex;
+        margin-top: 3rem;
+        justify-content: space-between;
+    }
+`
+
+export const FacilitieBox = styled.div`
+    display: flex;
+    gap: 1rem;
+    max-width: 22rem;
+    
+    div{
+        display: flex;
+        flex-direction: column;
+        gap: 0.75rem;
+
+        span{
+            font-weight: bold;
+            color: ${props => props.theme['--green']}
+        }
+    }
+`
+
+export const NewsLetterArea = styled.section`
+    max-width: 900px;
+    margin: 4rem auto;
+    padding: 3rem;
+    border: 1px solid ${props => props.theme['--black']};
+    
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+
+    h2{
+        text-align: center;
+    }
+
+    form{
+        display: flex;
+        gap: 1rem;
+
+        input{
+            flex: 1;
+            padding: 0.5rem 1rem;
+        }
+
+        button{
+            border: 0;
+            padding: 0 1rem;
+            background-color: ${props => props.theme['--purple']};
+            color: ${props => props.theme['--white']};
+            font-weight: bold;
+        }
+    }
+`
+
 export const FooterConteiner = styled.footer`
     background-color: ${props => props.theme['--black']};
     color: ${props => props.theme['--white']};
     text-align: center;
-    margin-top: 3rem;
 
     padding: 1rem 0;
 `
